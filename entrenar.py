048209f219757a20e378e40184a3a0eae7cffd95
Marian Herrera <64176067+saalej@users.noreply.github.com>
2021-09-17 02:40:02 -0500

    Update entrenar.py

--- entrenar.py
+++ entrenar.py
@@ -5,26 +5,25 @@ Created on Thu Sep 16 21:21:57 2021
 @author: A00227534
 emma g alfaro a01740229
 """
-import sys
 import os
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.python.keras import optimizers
-from tensorflow.python.keras.models import Sequentials
-from tensorflow.python.keras.layers import Dropout, Flatten, Dense, Activation
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dropout, Flatten, Dense
 from tensorflow.python.keras.layers import Convolution2D, MaxPooling2D
 from tensorflow.python.keras import backend as K
 
 K.clear_session()
 
-dataEntrenamiento = './data/entrenamiento' # Direccion de imagenes para entrenar
-dataValidacion = '.data/validacion' # Direccion de imagenes para validar
+data_entrenamiento = './data/entrenamiento' # Direccion de imagenes para entrenar
+data_validacion = './data/validacion' # Direccion de imagenes para validar
 
 
 # Parametros
 epocas = 20
 altura, longitud = 100, 100
 
-batchSize = 32
+batch_size = 32
 pasos = 1000
 pasosValidacion = 200
 filtrosConvl1 = 32
@@ -38,9 +37,9 @@ lr = 0.005
 # preprocesamiento de imagenes
 entrenamientoDatagen = ImageDataGenerator(
        rescale = 1./255, 
-       shearRange = 0.3,
-       zoomRange = 0.3,
-       horizontalFlip = True
+       shear_range = 0.3,
+       zoom_range = 0.3,
+       horizontal_flip = True
     )
 
 validacionDatagen = ImageDataGenerator(
@@ -48,37 +47,43 @@ validacionDatagen = ImageDataGenerator(
     )
 
 imagenEntrenamiento = entrenamientoDatagen.flow_from_directory(
-        dataEntrenamiento,
-        targetSize = (altura, longitud),
-        batchSize = batchSize,
-        classMode = 'categorial'
+        data_entrenamiento,
+        target_size = (altura, longitud),
+        batch_size = batch_size,
+        class_mode = 'categorical'
     )
 
 imagenValidacion = validacionDatagen.flow_from_directory(
-        dataValidacion,
-        targetSize = (altura, longitud),
-        batchSize = batchSize,
-        classMode = 'categorial'
+        data_validacion,
+        target_size = (altura, longitud),
+        batch_size = batch_size,
+        class_mode = 'categorical'
     )
 
 # Crear la red CNN
-cnn = Sequentials()
+cnn = Sequential()
 
-cnn.add(Convolution2D(filtrosConvl1, tamFiltro1, padding = 'same', inputShape = (altura, longitud, 3), activation = 'relu'))
-
-cnn.add(MaxPooling2D(poolSize = tamPool))
+cnn.add(Convolution2D(filtrosConvl1, tamFiltro1, padding = 'same', input_shape = (altura, longitud, 3), activation = 'relu'))
+cnn.add(MaxPooling2D(pool_size = tamPool))
 
 cnn.add(Convolution2D(filtrosConvl2, tamFiltro2, padding = 'same', activation = 'relu'))
-cnn.add(MaxPooling2D(poolSize = tamPool))
+cnn.add(MaxPooling2D(pool_size = tamPool))
 
 cnn.add(Flatten())
 cnn.add(Dense(256, activation = 'relu'))
 cnn.add(Dropout(0.5))
 cnn.add(Dense(clases, activation = 'softmax'))
 
-cnn.compile(loss='categorical_crossetropy', optimizer = optimizers.Adam(lr=lr), metrics=['accuracy'])
+cnn.compile(loss='categorical_crossentropy',
+            optimizer = optimizers.Adam(lr=lr),
+            metrics=['accuracy'])
 
-cnn.fit(imagenEntrenamiento,steps_per_epoch = pasos, epochs = epocas, validacionDatagen = imagenValidacion, validationsSteps = pasosValidacion)
+cnn.fit(imagenEntrenamiento,
+        steps_per_epoch = pasos, 
+        epochs = epocas, 
+        validacion_data = imagenValidacion, 
+        pasosValidacion = pasosValidacion
+    )
 
 dir = './modelo/'
 
@@ -86,7 +91,4 @@ if not os.path.exists(dir):
     os.mkdir(dir)
 cnn.save('.modelo/modelo.h5')
 cnn.save_weights('.modelo/pesos.h5')
-
-
-
 

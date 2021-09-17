e843b4dd78242d2843eecb49b0b086b77760b43c
Marian Herrera <64176067+saalej@users.noreply.github.com>
2021-09-16 22:43:22 -0500

    Update entrenar.py

--- entrenar.py
+++ entrenar.py
@@ -9,6 +9,7 @@ import os
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.python.keras import optimizers
 from tensorflow.python.keras.models import Sequentials
+from tensorflow.python.keras.layers import Dropout, Flatten, Dense, Activation
 from tensorflow.python.keras.layers import Convolution2D, MaxPooling2D
 from tensorflow.python.keras import backend as K
 

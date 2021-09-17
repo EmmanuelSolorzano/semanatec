1d821d1784f6cd93ce5f01698d7157a741d8cf55
Marian Herrera <64176067+saalej@users.noreply.github.com>
2021-09-17 02:45:24 -0500

    Update predecir.py

--- predecir.py
+++ predecir.py
@@ -6,12 +6,12 @@ longitud, altura = 100, 100
 modelo = './modelo/modelo.h5'
 pesos = './modelo/pesos.h5'
 
-cnn=load_model(modelo)
+cnn = load_model(modelo)
 cnn.load_weights(pesos)
 
 def predict(file):
     x = load_img(file, target_size=(longitud,altura))
-    x = img_to_array
+    x = img_to_array(x)
     x = np.expand_dims(x, axis = 0)
     arreglo = cnn.predict(x)##[[1,0,0]]
     resultado = arreglo[0] ##[1,0,0]
@@ -21,10 +21,12 @@ def predict(file):
     elif respuesta==1:
         print('Gato')
     elif respuesta == 2:
-        print('Gorila')
+        print('Mujer')
+    elif respuesta == 3:
+        print("Hombre")
     return respuesta
 
 
-predict("aqui se pone las imagenes")
+predict("Camila.jpg")
 
 
